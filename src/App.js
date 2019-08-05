d7c56ca0460993d98c24e11f51b9b9dc2bbbdc43
smellyung <kelly.ung@centrica.com>
2019-08-05 12:41:57 +0100

    change header text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Kelly's Kodflix App yaaaaasss</h1>
+        <h1>Kelly's Kodflix App yaaaaass</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
