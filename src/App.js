07208b67d410c19b42c45b1468f6c8dd5461d621
smellyung <kelly.ung@centrica.com>
2019-08-06 16:58:58 +0100

    replace tv show titles with covers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import theRaidPoster from './Raod-Poster.jpg';
+import blackMirrorPoster from './images/black-mirror.jpg';
+import breakingBadPoster from './images/breaking-bad.jpg';
+import deathNotePoster from './images/death-note.jpg';
+import gameOfThronesPoster from './images/game-of-thrones.jpg';
+import theWalkingDeadPoster from './images/the-walking-dead.jpg';
+import theWirePoster from './images/the-wire.jpg';  
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src={theRaidPoster} alt="the-raid-poster" />
-
       <div className="container">
-        <div className="movie">Black Mirror</div>
-        <div className="movie">Breaking Bad</div>
-        <div className="movie">Death Note</div>
+        <div className="movie">
+          <img src={blackMirrorPoster} alt="black-mirror-poster" />
+        </div>
+        <div className="movie">
+          <img src={breakingBadPoster} alt="breaking-bad-poster" />
+        </div>
+        <div className="movie">
+          <img src={deathNotePoster} alt="death-note-poster" />
+        </div>
       </div>
       <div className="container">
-        <div className="movie">Game of Thrones</div>
-        <div className="movie">The Walking Dead</div>
-        <div className="movie">The Wire</div>
+        <div className="movie">
+          <img src={gameOfThronesPoster} alt="game-of-thrones-poster" />
+        </div>
+        <div className="movie">
+         <img src={theWalkingDeadPoster} alt="the-walking-dead-poster" />
+        </div>
+        <div className="movie">
+         <img src={theWirePoster} alt="the-wire-poster" />
+        </div>
       </div>
 
     </div>
