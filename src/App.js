a27ef7158667585a48d1ee00f7b85a9e1d45579f
smellyung <kelly.ung@centrica.com>
2019-08-07 12:07:07 +0100

    add movie/show title overlay on hover

--- src/App.js
+++ src/App.js
@@ -13,23 +13,41 @@ function App() {
       <div className="container">
         <div className="movie">
           <img src={blackMirrorPoster} alt="black-mirror-poster" />
+          <div className="title-overlay">
+            <p>Black Mirror</p>
+          </div>
         </div>
         <div className="movie">
           <img src={breakingBadPoster} alt="breaking-bad-poster" />
+          <div className="title-overlay">
+            <p>Breaking Bad</p>
+            </div>
         </div>
         <div className="movie">
           <img src={deathNotePoster} alt="death-note-poster" />
+          <div className="title-overlay">
+            <p>Death Note</p>
+          </div>
         </div>
       </div>
       <div className="container">
         <div className="movie">
           <img src={gameOfThronesPoster} alt="game-of-thrones-poster" />
+          <div className="title-overlay">
+            <p>Game of Thrones</p>
+          </div>
         </div>
         <div className="movie">
          <img src={theWalkingDeadPoster} alt="the-walking-dead-poster" />
+         <div className="title-overlay">
+           <p>The Walking Dead</p>
+          </div>
         </div>
         <div className="movie">
          <img src={theWirePoster} alt="the-wire-poster" />
+         <div className="title-overlay">
+           <p>The Wire</p>
+          </div>
         </div>
       </div>
 
