50eef741d539aa2e6d0d7c48bbc8ddb4b16c728c
smellyung <kelly.ung@centrica.com>
2019-08-07 21:38:11 +0100

    use Gallery Component in App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 import React from 'react';
-import Movie from './Movie.js';
-import blackMirrorPoster from './images/black-mirror.jpg';
-import breakingBadPoster from './images/breaking-bad.jpg';
-import deathNotePoster from './images/death-note.jpg';
-import gameOfThronesPoster from './images/game-of-thrones.jpg';
-import theWalkingDeadPoster from './images/the-walking-dead.jpg';
-import theWirePoster from './images/the-wire.jpg';  
+import Gallery from './Gallery';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Movie
-          title="Black Mirror"
-          logo={blackMirrorPoster} />
-        <Movie
-          title="Breaking Bad"
-          logo={breakingBadPoster} />
-        <Movie
-          title="Death Note"
-          logo={deathNotePoster} />
-      </div>
-      <div className="container">
-        <Movie
-          title="Game of Thrones"
-          logo={gameOfThronesPoster} />
-        <Movie
-        title="The Walking Dead"
-        logo={theWalkingDeadPoster} />
-        <Movie
-        title="The Wire"
-        logo={theWirePoster} />
-      </div>
+      <Gallery />
     </div>
   );
 }
