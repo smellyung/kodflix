d690dcf9fb4aa9dc4a94df9e1f99131a48c3fc56
smellyung <kelly.ung@centrica.com>
2019-08-08 14:14:39 +0100

    implement routing/navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Gallery from './Gallery';
+import Details from './Details';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Gallery />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Gallery}/>
+        <Route exact path="/details" component={Details} />
+      </div>
+    </Router>
   );
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Details from './Details';
+import { Link } from "react-router-dom";
+
 
 export default function Movie(props) {
     return (
         <div className="movie">
-            <img src={props.logo} alt={`${props.title} logo`} />
+            <Link to="/details">
+                <img src={props.logo} alt={`${props.title} logo`} />
+            </Link>
             <div className="title-overlay">
                 <p>{props.title}</p>
             </div>
