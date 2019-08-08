9e7cc0659bf7264524958d21548da8d58fa91a40
smellyung <kelly.ung@centrica.com>
2019-08-08 14:13:30 +0100

    create Details component

---
+++ src/Details.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Details() {
+    return (
+        <div>
+            <h3>Details Page</h3>
+            <p>Hello, this will be the details page for each Movie & TV show :)</p>
+        </div>
+    );
+}
