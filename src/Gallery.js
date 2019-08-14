58b0c4e69ab7ba12de16eeec38efc276f2ef0158
smellyung <kelly.ung@centrica.com>
2019-08-14 15:38:25 +0100

    move covers data model to a proper front-end data source

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import Movie from './Movie.js';
-import blackMirrorPoster from './images/black-mirror.jpg';
-import breakingBadPoster from './images/breaking-bad.jpg';
-import deathNotePoster from './images/death-note.jpg';
-import gameOfThronesPoster from './images/game-of-thrones.jpg';
-import theWalkingDeadPoster from './images/the-walking-dead.jpg';
-import theWirePoster from './images/the-wire.jpg';  
+import getGallery from './gallery-get'; 
 
 export default function Gallery(props) {
   return (
     <> 
       <div className="container">
-        <Movie title="Black Mirror" logo={blackMirrorPoster} id="black-mirror" />
-        <Movie title="Breaking Bad" logo={breakingBadPoster} id="breaking-bad" />
-        <Movie title="Death Note" logo={deathNotePoster} id="death-note" />
-        <Movie title="Game of Thrones" logo={gameOfThronesPoster} id="game-of-thrones" />
-        <Movie title="The Walking Dead" logo={theWalkingDeadPoster} id="the-walking-dead" />
-        <Movie title="The Wire" logo={theWirePoster} id="the-wire" />
+        {
+          getGallery().map(movie => (
+              <Movie 
+                key={movie.id}
+                title={movie.title} 
+                logo={movie.logo} 
+                id={movie.id} />
+          ))
+        }
       </div>
     </>
   );
