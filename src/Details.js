8f01a46008e9f169566ca1dee77b8f9ec1e3719f
smellyung <kelly.ung@centrica.com>
2019-08-16 12:57:58 +0100

    introduce component state using class lifecycle methods

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Details({ match }) {
-    return (
-        <div>
-            <p>{match.params.id}</p>
-            <p>Hello, this will be the details page for each Movie & TV show :)</p>
-        </div>
-    );
+export default class Details extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            message: 'Hello, this will be the details page for each Movie & TV show :)'
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                message: 'Coming soon! :)'
+            })
+        }, 3000);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.message}</p>
+            </div>
+        )
+    }
+
 }
+
+// functional component draft
+// import React, { useState, useEffect } from 'react';
+
+// export default function Details({ match }) {
+//     const [detailsMessage, setDetailsMessage] = useState(
+//             'Hello, this will be the details page for each Movie & TV show :)'
+//         );
+    
+//     useEffect(
+//         setTimeout(setDetailsMessage('Coming soon! :)'), 3000)
+//     );
+
+//     return (
+//         <div>
+//             <p>{match.params.id}</p>
+//             <p>{detailsMessage}</p>
+//         </div>
+//     );
+// }
