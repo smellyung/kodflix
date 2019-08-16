00a2f08a95ead52ead380e28a33c8465b49bceea
smellyung <kelly.ung@centrica.com>
2019-08-16 12:53:34 +0100

    fix indentation in gallery-get component

--- src/gallery-get.js
+++ src/gallery-get.js
@@ -6,12 +6,12 @@ import theWalkingDeadPoster from './images/the-walking-dead.jpg';
 import theWirePoster from './images/the-wire.jpg'; 
 
 export default function getGallery() {
-    return [
-      {title: 'Black Mirror', logo: blackMirrorPoster, id: 'black-mirror'},
-      {title: 'Breaking Bad', logo: breakingBadPoster, id: 'breaking-bad'},
-      {title: 'Death Note', logo: deathNotePoster, id: 'death-note'},
-      {title: 'Game of Thrones', logo: gameOfThronesPoster, id: 'game-of-thrones'},
-      {title: 'The Walking Dead', logo: theWalkingDeadPoster, id: 'the-walking-dead'},
-      {title: 'The Wire', logo: theWirePoster, id: 'the-wire'}
-    ]
-  }
+  return [
+    {title: 'Black Mirror', logo: blackMirrorPoster, id: 'black-mirror'},
+    {title: 'Breaking Bad', logo: breakingBadPoster, id: 'breaking-bad'},
+    {title: 'Death Note', logo: deathNotePoster, id: 'death-note'},
+    {title: 'Game of Thrones', logo: gameOfThronesPoster, id: 'game-of-thrones'},
+    {title: 'The Walking Dead', logo: theWalkingDeadPoster, id: 'the-walking-dead'},
+    {title: 'The Wire', logo: theWirePoster, id: 'the-wire'}
+  ]
+}
