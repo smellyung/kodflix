8ea6b505f2aacd294d63125e12b0b0dd110bcc45
smellyung <kelly.ung@centrica.com>
2019-08-16 14:42:22 +0100

    remove commented out code

--- src/Details.js
+++ src/Details.js
@@ -26,23 +26,3 @@ export default class Details extends Component {
     }
 
 }
-
-// functional component draft
-// import React, { useState, useEffect } from 'react';
-
-// export default function Details({ match }) {
-//     const [detailsMessage, setDetailsMessage] = useState(
-//             'Hello, this will be the details page for each Movie & TV show :)'
-//         );
-    
-//     useEffect(
-//         setTimeout(setDetailsMessage('Coming soon! :)'), 3000)
-//     );
-
-//     return (
-//         <div>
-//             <p>{match.params.id}</p>
-//             <p>{detailsMessage}</p>
-//         </div>
-//     );
-// }
