0bb90fcaad3f1641c8c8084303cdd6514b663feb
smellyung <kelly.ung@centrica.com>
2019-08-21 11:38:59 +0100

    change details component to use function hooks

--- src/Details.js
+++ src/Details.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default class Details extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            message: 'Hello, this will be the details page for each Movie & TV show :)'
-        };
-    }
-
-    componentDidMount() {
+export default function Details() {
+    const [detailsMessage, setDetailsMessage] = useState(
+            'Hello, this will be the details page for each Movie & TV show :)'
+        );
+    
+    useEffect(() => {
         setTimeout(() => {
-            this.setState({
-                message: 'Coming soon! :)'
-            })
+            setDetailsMessage('Coming soon! :)')
         }, 3000);
-    }
+    });
 
-    render() {
-        return (
-            <div>
-                <p>{this.state.message}</p>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <p>{detailsMessage}</p>
+        </div>
+    );
 }
