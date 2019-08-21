c6368cf1d920bdaf785d52ed2bf89e3ed7bf8774
Kelly Ung <kellyung@hotmail.co.uk>
2019-08-21 09:54:26 +0100

    Feature/introduce component state (#1)
    * fix indentation in gallery-get component
    
    * introduce component state using class lifecycle methods
    
    * remove commented out code
    
    * remove space after render method

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Details({ match }) {
-    return (
-        <div>
-            <p>{match.params.id}</p>
-            <p>Hello, this will be the details page for each Movie & TV show :)</p>
-        </div>
-    );
+export default class Details extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            message: 'Hello, this will be the details page for each Movie & TV show :)'
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                message: 'Coming soon! :)'
+            })
+        }, 3000);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.message}</p>
+            </div>
+        )
+    }
 }
