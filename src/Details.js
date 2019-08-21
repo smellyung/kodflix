9a7dc95c87e6269c56a7fcc21d4e35a67be41999
smellyung <kelly.ung@centrica.com>
2019-08-21 12:15:16 +0100

    show respective tv-show title on details page

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,19 @@
-import React, { useState, useEffect } from 'react';
 
-export default function Details() {
+import React, { useState, useEffect } from 'react';
+import getGallery from './gallery-get';
+
+export default function Details({ match }) {
     const [detailsMessage, setDetailsMessage] = useState(
             'Hello, this will be the details page for each Movie & TV show :)'
         );
-    
+    const [title, setTitle] = useState ('');
+    const movieId = match.params.id;
+    const movieTitle = getGallery().find(movie => 
+        movie.id === movieId
+    ).title;
+
     useEffect(() => {
+        setTitle(movieTitle);
         setTimeout(() => {
             setDetailsMessage('Coming soon! :)')
         }, 3000);
@@ -13,6 +21,7 @@ export default function Details() {
 
     return (
         <div>
+            <p>{title}</p>
             <p>{detailsMessage}</p>
         </div>
     );
