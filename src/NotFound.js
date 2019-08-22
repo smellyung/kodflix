c40928aa186d80cfe2498153b649351492f8325c
smellyung <kelly.ung@centrica.com>
2019-08-22 15:23:47 +0100

    create Not Found component

---
+++ src/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+    return (
+        <div>
+            <p>Ooops, it looks like this page doesn't exist :(</p>
+        </div>
+    );
+}
