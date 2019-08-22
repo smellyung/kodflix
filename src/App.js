0747dbaac653caa317261af43c4cb76ff4d13ab5
smellyung <kelly.ung@centrica.com>
2019-08-22 12:27:43 +0100

    rename path param to movieId for semantics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Gallery from './Gallery';
 import Details from './Details';
+import NotFound from './NotFound';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -10,7 +11,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Gallery}/>
-          <Route exact path="/:id" component={Details}/>
+          <Route exact path="/:movieId" component={Details}/>
         </Switch>
       </div>
     </Router>
