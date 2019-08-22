adad3aaaf65dacbb4f8a2eb73d31108680b112ef
smellyung <kelly.ung@centrica.com>
2019-08-22 15:29:31 +0100

    redirect to a not-found page when targeted show doesnt exist

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Gallery}/>
+          <Route exact path="/not-found" component={NotFound}/>
           <Route exact path="/:movieId" component={Details}/>
         </Switch>
       </div>
--- src/Details.js
+++ src/Details.js
@@ -1,28 +1,35 @@
 
 import React, { useState, useEffect } from 'react';
 import getGallery from './gallery-get';
+import { Redirect } from 'react-router-dom';
 
 export default function Details({ match }) {
     const [detailsMessage, setDetailsMessage] = useState(
             'Hello, this will be the details page for each Movie & TV show :)'
         );
-    const [title, setTitle] = useState ('');
-    const movieId = match.params.id;
-    const movieTitle = getGallery().find(movie => 
+    const [movie, setMovie] = useState ('');
+    const movieId = match.params.movieId;
+    const matchedMovie = getGallery().find(movie => 
         movie.id === movieId
-    ).title;
-
+    );
+    
     useEffect(() => {
-        setTitle(movieTitle);
+        setMovie(matchedMovie);
         setTimeout(() => {
             setDetailsMessage('Coming soon! :)')
         }, 3000);
     });
 
-    return (
-        <div>
-            <p>{title}</p>
-            <p>{detailsMessage}</p>
-        </div>
-    );
+    if (movie === undefined) {
+        return (
+            <Redirect to='/not-found' />
+        );
+    } else {
+        return (
+            <div>
+                <p>{movie.title}</p>
+                <p>{detailsMessage}</p>
+            </div>
+        );
+    }
 }
