e8624078891497da77bf0d4d8be215ec54594be4
smellyung <kelly.ung@centrica.com>
2019-08-28 15:52:07 +0100

    connect frontend and backend servers

--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Movie from './Movie.js';
 import getGallery from './gallery-get'; 
 
 export default function Gallery(props) {
+  useEffect(() => {
+    fetch('/rest/gallery')
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+          console.log(myJson);
+      })
+  });
   return (
     <> 
       <div className="container">
