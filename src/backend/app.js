8f52f3fd0e4bb28f26c1e49b68b18748ea41b39d
smellyung <kelly.ung@centrica.com>
2019-08-28 10:59:31 +0100

    create backend folder and resources

---
+++ src/backend/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const getGallery = require('./gallery')
+
+app.get('/rest/gallery', (req, res) => res.send(getGallery()))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
