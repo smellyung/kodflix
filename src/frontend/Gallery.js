fe231d02bd96faf35474a8773fc3a74955aa248d
smellyung <kelly.ung@centrica.com>
2019-08-29 17:55:18 +0100

    add comments to explain each step and change json response for semantics

--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import Movie from './Movie.js';
 
-export default function Gallery(props) {
+export default function Gallery() {
   const [gallery, setGallery] = useState([]);
   useEffect(() => {
+    //fetch data in gallery backend
     fetch('/rest/gallery')
       .then(response => response.json())
-      .then(movies => setGallery(movies))
+      .then(backendGallery => setGallery(backendGallery))
   }, []);
   return (
     <> 
       <div className="container">
         {
+          //render each movie using Movie Component
           gallery.map(movie => (
               <Movie 
                 key={movie.id}
