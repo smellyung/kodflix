4a52d6306e8ca39d2bc40f7c69049e33dbb87da0
smellyung <kelly.ung@centrica.com>
2019-08-29 11:00:42 +0100

    add semi colons

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
-const app = express()
+const app = express();
 const port = process.env.PORT || 3001;
-const getGallery = require('./gallery')
+const getGallery = require('./gallery');
 
-app.get('/rest/gallery', (req, res) => res.send(getGallery()))
-app.use(express.static(path.join(__dirname, '../../build')))
+app.get('/rest/gallery', (req, res) => res.send(getGallery()));
+app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
