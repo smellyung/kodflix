c47082a5988d772c14698ee08178a4db5bb46ef0
smellyung <kelly.ung@centrica.com>
2019-08-29 15:45:56 +0100

    introduce state to gallery component and fetch data from backend

--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -1,26 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Movie from './Movie.js';
-import getGallery from './gallery-get'; 
 
 export default function Gallery(props) {
+  const [gallery, setGallery] = useState([]);
   useEffect(() => {
     fetch('/rest/gallery')
-      .then((response) => {
-        return response.json();
-      })
-      .then((myJson) => {
-          console.log(myJson);
-      })
+      .then(response => response.json())
+      .then(movies => setGallery(movies))
   });
   return (
     <> 
       <div className="container">
         {
-          getGallery().map(movie => (
+          gallery.map(movie => (
               <Movie 
                 key={movie.id}
                 title={movie.title} 
-                poster={movie.poster}
                 id={movie.id} />
           ))
         }
